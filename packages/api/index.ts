08d8b2e82e8feef60822a379b4497b3a42676beb
kreako <olivier@kreako.fr>
2022-02-12 14:08:15 +0100

    Upgrade api type

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,10 +1,23 @@
-type Word = {
+import { z } from "zod"
+
+// words
+export const wordsInput = z.object({
+  id: z.string(),
+})
+export type WordsInput = z.infer<typeof wordsInput>
+
+export type WordType = {
   start: number
   end: number
   conf: number
   word: string
 }
 
-export type VideoOutput = {
-  words: Word[]
-}
+export const wordsOutput = (words: { words: WordType[] }) => words
+export type WordsOutput = ReturnType<typeof wordsOutput>
+
+// video
+export const videoInput = z.object({
+  id: z.string(),
+})
+export type VideoInput = z.infer<typeof videoInput>

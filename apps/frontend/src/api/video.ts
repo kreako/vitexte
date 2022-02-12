634375727261eea8dc640ef0a16518eb13d436d4
kreako <olivier@kreako.fr>
2022-02-12 14:09:35 +0100

    video and words query wrapper

--- apps/frontend/src/api/video.ts
+++ apps/frontend/src/api/video.ts
@@ -1,12 +1,25 @@
-import type { VideoOutput } from "@vitexte/api"
-import { useMutation, useQuery, useQueryClient } from "react-query"
-import { keysVideo } from "./query-key"
+import type { WordsOutput } from "@vitexte/api"
+import { QueryFunctionContext, useMutation, useQuery, useQueryClient } from "react-query"
+import { keys } from "./query-key"
 import { get, put, retryQuery, UseMutationType } from "./utils"
 
-const fetchVideo = async (): Promise<VideoOutput> => get("/video/1")
+const fetchWords = async ({
+  queryKey: [{ id }],
+}: QueryFunctionContext<ReturnType<typeof keys["words"]>>): Promise<WordsOutput> => get("/words/1")
 
-export const useVideo = () => {
-  return useQuery(keysVideo.video, fetchVideo, {
+export const useWords = (id: number) => {
+  return useQuery(keys.words(id), fetchWords, {
+    useErrorBoundary: true,
+    retry: retryQuery(["AuthenticationError"]),
+  })
+}
+
+const fetchVideo = async ({
+  queryKey: [{ id }],
+}: QueryFunctionContext<ReturnType<typeof keys["video"]>>): Promise<WordsOutput> => get("/video/1")
+
+export const useVideo = (id: number) => {
+  return useQuery(keys.video(id), fetchVideo, {
     useErrorBoundary: true,
     retry: retryQuery(["AuthenticationError"]),
   })
